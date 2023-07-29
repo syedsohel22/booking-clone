e74d31f4623df896f50df75950b1e83bb9d9b84d
sohel <mr.sohelsyed@gmail.com>
2023-07-29 23:20:43 +0530

    error.js

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,9 +1,10 @@
 import express from "express";
 import Hotel from "../models/Hotel.js";
+import { createError } from "../utils/error.js";
 const router = express.Router();
 
 //Create
-router.post("/", );
+router.post("/");
 //update
 router.put("/:id", async (req, res) => {
   try {
@@ -42,7 +43,7 @@ router.get("/:id", async (req, res) => {
 //get all
 router.get("/", async (req, res, next) => {
   try {
-    const hotel = await Hotel.findById("dfg");
+    const hotel = await Hotel.find();
     res.status(200).json(hotel);
   } catch (error) {
     next(error);
