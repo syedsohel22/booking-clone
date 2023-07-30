e0b0bf438b80fc9fd7ac3e6be25c38ca4222335b
sohel <mr.sohelsyed@gmail.com>
2023-07-30 23:15:00 +0530

    working on login route with jwt and cookies

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import authRoute from "./routes/auth.js";
 import usersRoute from "./routes/users.js";
 import hotelsRoute from "./routes/hotels.js";
 import roomsRoute from "./routes/rooms.js";
+import cookieParser from "cookie-parser";
 const app = express();
 dotenv.config();
 
@@ -28,11 +29,11 @@ mongoose.connection.on("connected", () => {
 // middleware
 
 app.use(express.json());
+app.use(cookieParser());
 app.use("/api/auth", authRoute);
 app.use("/api/users", usersRoute);
 app.use("/api/hotels", hotelsRoute);
 app.use("/api/rooms", roomsRoute);
-
 app.get("/", (req, res) => {
   res.send("Welcome to Booking app api");
 });
