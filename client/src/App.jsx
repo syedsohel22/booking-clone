beac294fd4fd4b67f97f6041ba7787abdf5840a6
sohel <mr.sohelsyed@gmail.com>
2023-09-29 01:08:21 +0530

    buidling some basic layouts and settting up routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,21 @@
-import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home/Home";
+import List from "./pages/list/List";
+import Hotel from "./pages/Hotel/Hotel";
+import Login from "./pages/Login/Login";
 
 function App() {
-  return <>Hello world.!</>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/hotels" element={<List />} />
+        <Route path="/hotels/:id" element={<Hotel />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
